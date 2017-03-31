11aed348a07d3844e9d1770970e9a2b3e6a50e13
Marián Hlaváč <hlavac95@gmail.com>
2017-03-31 12:59:38 +0200

    Create a few testing page components for testing purposes

---
+++ src/pages/Timetable.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>Timetable page<br>
+    Go to <a href="/settings">the settings page</a>.
+  </div>
+</template>
+
+<script>
+  export default {
+    name: 'Timetable',
+  }
+</script>
